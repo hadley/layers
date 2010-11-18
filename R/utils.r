042fef2bf883b2f63c690900b9539becbb1aee4f
hadley <h.wickham@gmail.com>
2010-11-18 14:25:22 -0600

    Extra util functions we need

---
+++ R/utils.r
@@ -0,0 +1,14 @@
+"%||%" <- function(a, b) {
+  if (!is.null(a)) a else b
+}
+
+resolution <- function(x, zero = TRUE) {
+  x <- unique(as.numeric(x))
+  if (length(x) == 1) return(1)
+
+  if (zero) {
+    x <- unique(c(0, x))
+  }
+  
+  min(diff(sort(x)))
+}
