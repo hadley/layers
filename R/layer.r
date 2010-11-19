78649c1de183de075c0520535f287706d01d2183
hadley <h.wickham@gmail.com>
2010-11-19 09:23:25 -0600

    Remove draft layer code for now

--- R/layer.r
+++ R/layer.r
@@ -6,23 +6,6 @@
 #' @aliases layers package-layers
 NULL
 
-layer_path <- function(arrow = NULL, lineend = "butt", linejoin = "round", linemitre = 1, na.rm = FALSE, ...) {
-  new_layer(..., geom = geom_from_layer("path"))
-}
-
-layer_point <- function(..., na.rm = FALSE) {
-  new_layer(..., geom = geom_from_layer("point"))
-}
-
-layer_rect <- function(...) {
-  new_layer(..., geom = geom_from_layer("rect"))
-}
-
-layer_bar <- function(width = NULL, na.rm = FALSE, ...) {
-  new_layer(..., geom = geom_from_layer("bar"))
-}
-
-
 geom_from_layer <- function(name, call = NULL) {
   if (is.null(call)) {
     call <- match.call(sys.call(sys.parent(2)), expand.dots = FALSE)
