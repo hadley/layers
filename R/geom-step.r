7d2bac3eef1dd92441d9f609f8a407d79feaff0f
hadley <h.wickham@gmail.com>
2010-11-19 13:45:33 -0600

    Add geom_step

---
+++ R/geom-step.r
@@ -0,0 +1,50 @@
+#' Connect observations with stairs.
+#'
+#' Equivalent to plot(type='s').
+#'
+#' @param direction = "direction of stairs: 'vh' for vertical then horizontal,
+#'    or 'hv' for horizontal then vertical"
+#' @export
+#' @S3method geom_grob step
+#' @S3method geom_visualise step
+#' df <- data.frame(x = 1:10, y = 1:10)
+#' geom_plot(geom_step(), df)
+#' geom_plot(geom_step(direction = "vh"), df)
+geom_step <- function(aesthetics = list(), direction = "hv") {
+  geom_from_call(c("step", "path"))
+}
+
+geom_grob.step <- function(geom, data, ...) {
+  data <- as.data.frame(data, stringsAsFactors = FALSE)
+  data <- stairstep(data, geom$direction)
+  
+  # Call pass on to geom_grob.path
+  NextMethod()
+}
+
+geom_visualise.step <- function(geom, data = list()) {
+  defaults <- list(x = 1:15, y = 1:15)
+  data <- modifyList(defaults, data)
+
+  geom_grob(geom, data, default.units = "npc")
+}
+
+stairstep <- function(data, direction="hv") {
+  direction <- match.arg(direction, c("hv", "vh"))
+  data <- as.data.frame(data)[order(data$x), ]
+  n <- nrow(data)
+  
+  if (direction == "vh") {
+    xs <- rep(1:n, each = 2)[-2*n]
+    ys <- c(1, rep(2:n, each=2))
+  } else {
+    ys <- rep(1:n, each = 2)[-2*n]
+    xs <- c(1, rep(2:n, each=2))
+  }
+  
+  data.frame(
+    x = data$x[xs],
+    y = data$y[ys],
+    data[xs, setdiff(names(data), c("x", "y"))]
+  ) 
+}
