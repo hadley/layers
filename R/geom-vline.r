5f2a64d1b421aaf84a59d95e929bfb4e9ce56358
hadley <h.wickham@gmail.com>
2011-12-27 15:57:10 -0600

    Some geom_vline notes

---
+++ R/geom-vline.r
@@ -0,0 +1,27 @@
+# Challenge with geom_vline is that the vertical line should extend across
+# the full extent of the plot - the range of the coordinate system, not the
+# range of the scales.
+#
+# One way to solve the problem (as done by ggplot2) is to use infinties - 
+# drawing from the left to right edge of the plot at the grid level.  The
+# problem is that this only works for cartesian coordinate systems.
+#
+# Do we need sentinel values that represent special locations?
+# rel(0), rel(0.5), rel(1) would always be located at the 0
+# abs(1, "cm"), abs(-1, "cm")
+#
+# This needs enough S3 so that unit always carried along through all
+# mathematical transformations, and then carefully convert to data coordinates
+# at appropriate time? (Different times for rel and absolute)
+
+geom_vline <- function(aesthetics = list()) {
+  geom_from_call("vline")
+}
+
+
+geom_range.vline <- function(geom, data) {
+  list(x = range(data$intercept))
+}
+
+aes_required.vline <- function(geom, data) "intercept"
+aes_default.vline <- function(geom) build_defaults("line")
